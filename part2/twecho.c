ff7643d67af1088b201db270def858d276b42d32
Jae Woo Lee <jae@cs.columbia.edu>
2020-02-07 10:58:49 -0500

    Lab skeleton code

---
+++ part2/twecho.c
@@ -0,0 +1,40 @@
+/*
+ * twecho
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
+static char **duplicateArgs(int argc, char **argv)
+{
+
+}
+
+static void freeDuplicatedArgs(char **copy)
+{
+
+}
+
+/*
+ * DO NOT MODIFY main().
+ */
+int main(int argc, char **argv)
+{
+    if (argc <= 1)
+        return 1;
+
+    char **copy = duplicateArgs(argc, argv);
+    char **p = copy;
+
+    argv++;
+    p++;
+    while (*argv) {
+        printf("%s %s\n", *argv++, *p++);
+    }
+
+    freeDuplicatedArgs(copy);
+
+    return 0;
+}
